ac56a79a8929052be64fd479e9f1b44d3ef3ca51
ChrisDeath <christoph.callies@gmail.com>
2026-02-03 01:57:30 +0100

    Changes codec to h265 and fps 30 to save some space

--- src/moviemaker.sh
+++ src/moviemaker.sh
@@ -9,7 +9,7 @@ date="$(date "+%Y-%m-%d" -d "1 day ago")"
 mkdir -p ${RAW_IMAGE_DIR}/${date}
 mv ${RAW_IMAGE_DIR}/${date}_* ${RAW_IMAGE_DIR}/${date}/
 
-ffmpeg -f image2 -i ${RAW_IMAGE_DIR}/${date}/%*.jpg -vcodec libx264 -r 60 -strftime 1 ${PROCESSED_VID_DIR}/"${date}-${CAMERA_NAME}".mp4 -y
+ffmpeg -f image2 -i ${RAW_IMAGE_DIR}/${date}/%*.jpg -c:v libx265 -r 30 -strftime 1 ${PROCESSED_VID_DIR}/"${date}-${CAMERA_NAME}".mp4 -y
 
 #Clean out old data
 find ${RAW_IMAGE_DIR} -type d -ctime +${DAYS_TO_KEEP} -exec rm -rf '{}' \;
