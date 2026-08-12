6343562f2df631641b1a2734fb2b185a9123f622
ChrisDeath <christoph.callies@gmail.com>
2026-08-12 11:57:59 +0200

    Made imagepuller more accurate to take ffmpeg execution time into account

--- src/imagepuller.sh
+++ src/imagepuller.sh
@@ -12,6 +12,7 @@ if [ ! -z "${OVERLAY_TXT_FILE}" ] && [ -f "${OVERLAY_FONT_FILE}" ] ; then
 	for i in $(seq 0 $((IMAGESHOT_COUNT_MINUTE - 1)));
 	do 
 		SEC=$(printf "%02d" $((i * INTERVAL)))
+		START=$(date +%s)
 		ffmpeg -y -i ${CAMERA_RTSP} \
 		-vf drawtext="textfile=${CAMERA_HOME}/${OVERLAY_TXT_FILE} \
 		:fontfile=${CAMERA_HOME}/${OVERLAY_FONT_FILE} \
@@ -24,13 +25,18 @@ if [ ! -z "${OVERLAY_TXT_FILE}" ] && [ -f "${OVERLAY_FONT_FILE}" ] ; then
 		:reload=1" \
 		-rtsp_transport udp -r 25 -pix_fmt yuvj422p -an -frames:v 1 -strftime 1 \
 		"${RAW_IMAGE_DIR}/%Y-%m-%d_%H-%M-${SEC}.jpg"
-		[ $i -lt $((IMAGESHOT_COUNT_MINUTE - 1)) ] && sleep $((INTERVAL - 1))
+		ELAPSED=$(( $(date +%s) - START ))
+		SLEEP_TIME=$((INTERVAL - 1 - ELAPSED))
+		[ $i -lt $((IMAGESHOT_COUNT_MINUTE - 1)) ] && [ $SLEEP_TIME -gt 0 ] && sleep $SLEEP_TIME
 	done
 	else
 	for i in $(seq 0 $((IMAGESHOT_COUNT_MINUTE - 1)));
 	do 
 		SEC=$(printf "%02d" $((i * INTERVAL)))
+		START=$(date +%s)
 		ffmpeg -y -i ${CAMERA_RTSP} -rtsp_transport udp -r 25 -pix_fmt yuvj422p -vf scale=${IMAGE_RESOLUTION}:-2,setsar=1:1 -an -frames:v 1 -strftime 1 "${RAW_IMAGE_DIR}/%Y-%m-%d_%H-%M-${SEC}.jpg"
-		[ $i -lt $((IMAGESHOT_COUNT_MINUTE - 1)) ] && sleep $((INTERVAL - 1))
+		ELAPSED=$(( $(date +%s) - START ))
+		SLEEP_TIME=$((INTERVAL - 1 - ELAPSED))
+		[ $i -lt $((IMAGESHOT_COUNT_MINUTE - 1)) ] && [ $SLEEP_TIME -gt 0 ] && sleep $SLEEP_TIME
 	done
 fi
