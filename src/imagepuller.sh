e892e83fd38be5da3c14616d689ab093e2aa913b
ChrisDeath <christoph.callies@gmail.com>
2026-08-12 11:15:20 +0200

    Enhanced image puller by configurable amount of pictures per minute

--- src/imagepuller.sh
+++ src/imagepuller.sh
@@ -4,9 +4,14 @@ if [ ! -d "${RAW_IMAGE_DIR}" ] ; then
 	mkdir -p ${RAW_IMAGE_DIR}
 fi
 
+# Default to 2 screenshots per minute if not set
+IMAGESHOT_COUNT_MINUTE=${IMAGESHOT_COUNT_MINUTE:-2}
+INTERVAL=$((60 / IMAGESHOT_COUNT_MINUTE))
+
 if [ ! -z "${OVERLAY_TXT_FILE}" ] && [ -f "${OVERLAY_FONT_FILE}" ] ; then
-	for SEC in 00 30;
+	for i in $(seq 0 $((IMAGESHOT_COUNT_MINUTE - 1)));;
 	do 
+		SEC=$(printf "%02d" $((i * INTERVAL)))
 		ffmpeg -y -i ${CAMERA_RTSP} \
 		-vf drawtext="textfile=${CAMERA_HOME}/${OVERLAY_TXT_FILE} \
 		:fontfile=${CAMERA_HOME}/${OVERLAY_FONT_FILE} \
@@ -19,12 +24,13 @@ if [ ! -z "${OVERLAY_TXT_FILE}" ] && [ -f "${OVERLAY_FONT_FILE}" ] ; then
 		:reload=1" \
 		-rtsp_transport udp -r 25 -pix_fmt yuvj422p -an -frames:v 1 -strftime 1 \
 		"${RAW_IMAGE_DIR}/%Y-%m-%d_%H-%M-${SEC}.jpg"
-		sleep 29
+		[ $i -lt $((IMAGESHOT_COUNT_MINUTE - 1)) ] && sleep $((INTERVAL - 1))
 	done
 	else
-	for SEC in 00 30;
+	for i in $(seq 0 $((IMAGESHOT_COUNT_MINUTE - 1)));
 	do 
+		SEC=$(printf "%02d" $((i * INTERVAL)))
 		ffmpeg -y -i ${CAMERA_RTSP} -rtsp_transport udp -r 25 -pix_fmt yuvj422p -vf scale=${IMAGE_RESOLUTION}:-2,setsar=1:1 -an -frames:v 1 -strftime 1 "${RAW_IMAGE_DIR}/%Y-%m-%d_%H-%M-${SEC}.jpg"
-		sleep 29
+		[ $i -lt $((IMAGESHOT_COUNT_MINUTE - 1)) ] && sleep $((INTERVAL - 1))
 	done
 fi
